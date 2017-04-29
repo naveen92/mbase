eff80e1e8253839274a0107b29df35155c0f87f3
Naveen Kumar M <mail.naveen92@gmail.com>
2017-04-29 14:57:14 +0530

    Changes in GreeterTest.java

--- src/test/java/hello/GreeterTest.java
+++ src/test/java/hello/GreeterTest.java
@@ -1,7 +1,8 @@
-package main.java.hello;
+package test.java.hello;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
+import main.java.hello.*;
 
 import org.junit.Test;
 
