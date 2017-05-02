90cdd8b7a07cb57d1711f6185e58372819ee6c6a
Naveen Kumar M <mail.naveen92@gmail.com>
2017-05-02 20:56:42 +0530

    Updated POM file specifying start class

--- src/main/mbase/controller/IndexController.java
+++ src/main/mbase/controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Spring Boot Again..!";
     }
 
 }
